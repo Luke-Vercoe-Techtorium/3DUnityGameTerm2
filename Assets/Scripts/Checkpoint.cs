3e43b515d37862dfa756af2ec606899da98dfe02
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2024-05-21 12:14:52 +1200

    Added some todos

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,6 +5,14 @@ public class Checkpoint : MonoBehaviour
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent<PlayerWalking>(out var player))
-            player.RespawnPoint = this;
+            if (player.RespawnPoint != this)
+            {
+                player.RespawnPoint = this;
+                // TODO: reset lives
+            }
+            else
+            {
+                // TODO: subtract lives
+            }
     }
 }

a959dd95cfc7d6e1c1beae0ea64f60f1e003ff87
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2024-05-28 10:50:43 +1200

    Added pistons and end platform

---
+++ Assets/Scripts/Piston.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody PistonArm = null;
+    [SerializeField]
+    private float MoveDistance = 3.0f;
+    [SerializeField]
+    private float Offset = 0.0f;
+    [SerializeField]
+    private float Speed = 1.0f;
+
+    public void FixedUpdate()
+    {
+        float value = Mathf.Sin(Time.time * Speed + Offset);
+        PistonArm.MovePosition(transform.position + new Vector3(value * MoveDistance, 0.0f, 0.0f));
+    }
+}
